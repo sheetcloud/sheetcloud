1303d3bbb57f219633ca17018466e8293ce478fb
nicococo <sinnl0s>
2023-05-05 14:20:42 +0200

    Fixed an import error

--- sheetcloud/__init__.py
+++ sheetcloud/__init__.py
@@ -1,6 +1,13 @@
-import sheetcloud.sheets as sheets
-import sheetcloud.drive as drive
-import sheetcloud.orm as orm
-import sheetcloud.env as env
-import sheetcloud.templates as templates
-import sheetcloud.user as user
+# import sheetcloud.sheets as sheets
+# import sheetcloud.drive as drive
+# import sheetcloud.orm as orm
+# import sheetcloud.env as env
+# import sheetcloud.templates as templates
+# import sheetcloud.user as user
+
+from . import sheets
+from . import drive
+from . import orm
+from . import env
+from . import templates
+from . import user
